82d080a6343bf76a15cdb2e4d1bd250010e8b90c
Marat Gumerov <gumerovm74@gmail.com>
2020-07-08 23:21:33 +0300

    Add BootScene

--- assets/scripts/scenes/StartScene.js
+++ assets/scripts/scenes/StartScene.js
@@ -2,9 +2,7 @@ class StartScene extends Phaser.Scene {
     constructor() {
         super('Start');
     }
-    preload() {
-        this.load.image('bg', 'assets/sprites/background.png');
-    }
+    
     create() {
         this.createBackground();
     }
