85323106c9498f8099107cca06eae217e6cf8730
Marat Gumerov <gumerovm74@gmail.com>
2020-07-09 15:34:21 +0300

    player move

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -1,13 +1,18 @@
 class GameScene extends Phaser.Scene {
     constructor() {
-        super('Game')
+        super('Game');
+    }
+    init() {
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
     create() {
-        this.createBackground()
-        this.player = new Player(this)
-
+        this.createBackground();
+        this.player = new Player(this);
+    }
+    update() {
+        this.player.move();
     }
     createBackground() {
-        this.add.sprite(0, 0, 'bg').setOrigin(0)
+        this.add.sprite(0, 0, 'bg').setOrigin(0);
     }
 }
--- assets/scripts/scenes/StartScene.js
+++ assets/scripts/scenes/StartScene.js
@@ -1,15 +1,15 @@
 class StartScene extends Phaser.Scene {
     constructor() {
-        super('Start')
+        super('Start');
     }
     create() {
-        console.log('StartScene loaded')
-        this.createBackground()
-        this.createText()
-        this.setEvents()
+        console.log('StartScene loaded');
+        this.createBackground();
+        this.createText();
+        this.setEvents();
     }
     createBackground() {
-        this.add.sprite(0, 0, 'bg').setOrigin(0)
+        this.add.sprite(0, 0, 'bg').setOrigin(0);
     }
     createText() {
         this.add.text(config.width / 2, 500, 'Tap to start', {
@@ -19,7 +19,7 @@ class StartScene extends Phaser.Scene {
     }
     setEvents() {
         this.input.on('pointerdown', () => {
-            this.scene.start('Game')
+            this.scene.start('Game');
         });
     }
 }
