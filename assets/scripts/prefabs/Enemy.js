e490317ae3cab71ee5269658f5b0bd596c7268fb
Marat Gumerov <gumerovm74@gmail.com>
2020-07-10 10:57:34 +0300

    refactoring

--- assets/scripts/prefabs/Enemy.js
+++ assets/scripts/prefabs/Enemy.js
@@ -1,13 +1,15 @@
-class Enemy extends Enemy {
-  constructor(scene, x, y, texture, frame) {
-    super(scene, x, y, texture, frame)
-  }
-  init() {
-    super.init()
-    this.velocity = -250;
-  }
-  move() {
-    this.body.setVelocityX(this.velovity)
-
-  }
+class Enemy extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        this.init();
+    }
+    init() {
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        this.body.enable = true;
+        this.velocity = -250;
+    }
+    move() {
+        this.body.setVelocityX(this.velocity);
+    }
 }
