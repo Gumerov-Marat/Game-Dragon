239ac9e462efd797dee3ad318c7e40dc62e43c1d
Marat Gumerov <gumerovm74@gmail.com>
2020-07-10 23:20:28 +0300

    reactivation Sprites

--- assets/scripts/prefabs/Enemies.js
+++ assets/scripts/prefabs/Enemies.js
@@ -2,28 +2,36 @@ class Enemies extends Phaser.Physics.Arcade.Group {
     constructor(scene) {
         super();
         this.scene = scene;
+        this.countMax = 10;
+        this.countCreated = 0;
         this.count = 10;
         this.timer = this.scene.time.addEvent({
             delay: 1000,
             loop: true,
             callback: this.tick,
             callbackScope: this
-        })
+        });
     }
 
     tick() {
         console.log('cuttent length', this.getLength());
-        if (this.getLength() < this.count){
-            this.createEnemy()
+        if (this.countCreated() < this.countMax){
+            this.createEnemy();
         } else {
             console.log('remove timer');
-            this.timer.remove()
+            this.timer.remove();
         }
     }
 
     createEnemy() {
-        let enemy  = Enemy.generate(this.scene);
+        let enemy = this.getFirstDead();
+        if (!enemy){
+            enemy = Enemy.generate(this.scene);
+        } else {
+            enemy.reset();
+        }
         this.add(enemy);
         enemy.move();
+        ++this.countCreated;
     }
 }
