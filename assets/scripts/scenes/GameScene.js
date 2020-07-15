718001e1c2febe7ca4a6f58b5ef549c0b38c28e6
Marat Gumerov <gumerovm74@gmail.com>
2020-07-15 10:57:57 +0300

    add statistics

--- assets/scripts/scenes/GameScene.js
+++ assets/scripts/scenes/GameScene.js
@@ -4,6 +4,7 @@ class GameScene extends Phaser.Scene {
     }
     init() {
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.score = 0;
     }
     create() {
         this.createBackground();
@@ -11,27 +12,36 @@ class GameScene extends Phaser.Scene {
         this.enemies = new Enemies(this);
         this.createCompleteEvents();
         this.addOverlap();
+        this.createText();
     }
-
+    createText() {
+        this.scoreText = this.add.text(50, 50, 'Score: 0', {
+            font: '40px CurseCasual',
+            fill: '#FFFFFF'
+        });
+    }
     addOverlap() {
         this.physics.add.overlap(this.player.fires, this.enemies, this.onOverlap, undefined, this);
         this.physics.add.overlap(this.enemies.fires, this.player, this.onOverlap, undefined, this);
         this.physics.add.overlap(this.player, this.enemies, this.onOverlap, undefined, this);
     }
-
     onOverlap(source, target) {
+        if (source !== this.player && target !== this.player) {
+            ++this.score;
+            this.scoreText.setText(`Score: ${this.score}`);
+        }
         source.setAlive(false);
         target.setAlive(false);
     }
-
     createCompleteEvents() {
         this.player.once('killed', this.onComplete, this);
         this.events.once('enemies-killed', this.onComplete, this);
     }
-
     onComplete() {
-        this.scene.start('Start');
-        console.log(1);
+        this.scene.start('Start', {
+            score: this.score,
+            completed: this.player.active
+        });
     }
     update() {
         this.player.move();
